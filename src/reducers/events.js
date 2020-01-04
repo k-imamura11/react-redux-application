f388e6744ed13a91b7d5ed66bf2492442a3b887f
imamurak <imamurak@s-p-net.com>
2020-01-05 02:16:34 +0900

    eventå®è£

---
+++ src/reducers/events.js
@@ -0,0 +1,12 @@
+import _ from 'lodash'
+import { READ_EVENTS } from '../actions'
+
+export default (state = {}, action) => {
+    const events = {}
+    switch (action.type) {
+        case READ_EVENTS:
+            return _.mapKeys(action.response.data, 'id')
+        default:
+            return events
+    }
+}
